1ed30f7cf429460f24ce9b1d92011f82d295cb8e
zepeersGily <>
2023-06-02 11:43:18 +0200

    include homepage title and description

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,18 @@
 export default function Home() {
    return (
-      <div>Home</div>
+      <section className='w-full flex-col flex-center'>
+         <h1 className='head_text text-center'>
+            Discover & Share&nbsp;
+            <br className='max-md:hidden' />
+            <span className='orange_gradient'>
+               AI-Powered Prompts
+            </span>
+         </h1>
+         <p className='desc text-center'>
+         Discover, create and share creative prompts with Promptastic —
+         the open-source AI prompting tool
+         </p>
+         {/* Feed */}
+      </section>
    )
 }
