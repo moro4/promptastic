99d980378ccbe6f5838519b0b50517b0ab3762e1
zepeersGily <>
2023-06-15 17:25:32 +0200

    Add function to connect to MongoDB using Mongoose

---
+++ utils/database.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+let isDBConnected = false;
+
+export async function connectToDB() {
+   mongoose.set('strictQuery', true);
+
+   if(isDBConnected) {
+      console.log('MongoDB is already connected');
+      return
+   }
+
+   try {
+      await mongoose.connect(process.env.MONGODB_URI, {
+         dbName: 'share_prompt',
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+      });
+
+      isDBConnected = true;
+
+      console.log('MongoDB is connected');
+
+   } catch (error) {
+      console.log(error);
+   }
+}
