bdcb41e6d14523e32b15899bd40e3cb34f50dc36
zepeersGily <>
2023-06-30 11:42:38 +0200

    create a preliminary homepage feed component with partially implemented search feature and prompt display

--- components/feed.jsx
+++ components/feed.jsx
@@ -1,5 +1,56 @@
-export default function Feed() {
+'use client';
+
+import {useState, useEffect} from 'react';
+import PromptCard from './promptcard';
+
+function PromptCardList({data, handleTagClick}) {
    return (
-      <div>Hello World!</div>
+      <div className='mt-16 prompt_layout'>
+         {data.map(post => (
+            <PromptCard
+               key={post._id}
+               post={post}
+               handleTagClick={handleTagClick}
+            />
+         ))}
+      </div>
+   )
+}
+
+export default function Feed() {
+   const [searchText, setSearchText] = useState('');
+   const [posts, setPosts] = useState([]);
+
+   function handleSearchChange(e) {
+      console.log('handleInput');
+   }
+
+   useEffect(() => {
+      async function fetchPosts() {
+         const response = await fetch('/api/prompt');
+         const data = await response.json();
+         setPosts(data);
+      }
+      fetchPosts();
+   }, []);
+
+   return (
+      <section className='feed'>
+         <form className='relative w-full flex-center'>
+            <input
+               type="text"
+               placeholder='Search for a tag or a username'
+               value={searchText}
+               onChange={handleSearchChange}
+               required
+               className='search_input'
+            />
+         </form>
+
+         <PromptCardList
+            data={posts}
+            handleTagClick={() => {}}
+         />
+      </section>
    )
 }
