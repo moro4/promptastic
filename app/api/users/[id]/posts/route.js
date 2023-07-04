1a5ecf49a6231f40bfffae54b480f3746fd3e0a7
zepeersGily <>
2023-07-04 14:44:06 +0200

    define a route handler fetching all prompts from a specified user in mongoDB

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,14 @@
+import {connectToDB} from '@utils/database';
+import Prompt from '@models/prompt';
+
+export async function GET(reg, {params: {id}}) {
+   try {
+      await connectToDB();
+      const prompts = await Prompt.findOne({creator: id}).populate('creator');
+      return new Response(JSON.stringify(prompts), {status: 200});
+   } catch (error) {
+      return new Response(
+         JSON.stringify('Failed to fetch prompts', {status: 500})
+      );
+   }
+}
