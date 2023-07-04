0affb1be4ebd591e2fe6af019f3a75e4f8a9f3d9
zepeersGily <>
2023-07-04 15:36:31 +0200

    add a profile component displaying all prompts from a specific user

--- components/profile.jsx
+++ components/profile.jsx
@@ -1,5 +1,22 @@
-export default function Profile() {
+import PromptCard from './promptcard';
+
+export default function Profile({ name, desc, data, handleEdit, handleDelete }) {
    return (
-      <div>Hello World!</div>
+      <section className='w-full'>
+         <h1 className='head_text text-left'>
+            <span className='blue_gradient'>{name} Profile</span>
+         </h1>
+         <p className='desc text-left'>{desc}</p>
+         <div className='mt-10 prompt_layout'>
+            {data.map(post => (
+               <PromptCard
+                  key={post._id}
+                  post={post}
+                  handleEdit={handleEdit(post)}
+                  handleDelete={handleDelete(post)}
+               />
+            ))}
+         </div>
+      </section>
    )
 }
